e88b05cfe23b966cee237659109d0cb7ab01cc5d
Ali Mohammadpour <alimpk@outlook.com>
2015-08-09 10:35:55 +0430

    add Main File (Project Now Works Partially)

---
+++ src/MainD.java
@@ -0,0 +1,9 @@
+import Graphic.*;
+
+public class MainD {
+    public static void main(String[] args) {
+
+        MainFrame mainFrame =new MainFrame();
+        mainFrame.setVisible(true);
+    }
+}
